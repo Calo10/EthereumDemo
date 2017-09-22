6a89797ae5de864e30bb80ceaf89b63f03f69b3f
Carlos <carlos_mendez_10@live.com>
2017-09-22 00:41:22 +0000

    Commit Jose

---
+++ EthereumDemoHost/EthereumWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EthereumWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EthereumDemoHost/EthereumWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EthereumWeb.WebApiApplication" Language="C#" %>

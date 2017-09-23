d37aebeaffe3685e676b56040d44755b2ff153eb
Your Name <carlos_mendez_10@live.com>
2017-09-23 11:45:20 -0600

    Cambios Raykel

---
+++ EthereumDemoHost/EthereumWeb/Models/ProposalConnection.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EthereumWeb.Models
+{
+    public class ProposalConnection
+    {
+        Connection conexionM = new Connection();
+
+        public bool InserProposal(PoposalModel proposal)
+        {
+            bool isValid = true;
+
+            try
+            {
+
+                List<ParameterSchema> lstParams = new List<ParameterSchema>();
+                string query = string.Empty;
+
+                lstParams.Add(new ParameterSchema("ContracEthereumProposal", proposal.ContracEthereumProposal));
+                lstParams.Add(new ParameterSchema("ProposalName", proposal.ProposalName));
+                lstParams.Add(new ParameterSchema("ProposalDescription", proposal.ProposalDescription));
+
+
+
+                if (!string.IsNullOrEmpty(proposal.ContracEthereumProposal))
+                {
+                    //Inserta Nuevo Curso
+                    query = "INSERT INTO dbEthereumDemo.Proposal(ContracEthereumProposal,ProposalName,ProposalDescription)" +
+                            "VALUES(@ContracEthereumProposal, @NameProposal, @DescriptionProposal); ";
+
+
+                }
+                else
+                {
+                    query = "Update Curso Set Nombre= @Nombre,Descripcion = @Descripcion, Tipo_Curso= @Tipo_Curso, Nivel_Curso= @Nivel_Curso, PPT_Enbed= @PPT_Enbed, HTML_Code=@HTML_Code, Lineas_Codigo= @Lineas_Codigo, " +
+                                   "Youtube_URL = @Youtube_URL, Repositorio_GH= @Repositorio_GH, Horas_Video= @Horas_Video, Privacidad = @Privacidad,Codigo_Acceso = @Codigo_Acceso, Estado = @Estado, Autor = @Autor  where id = @id";
+
+                }
+
+
+                return conexionM.setExecuteQuery(query, lstParams).Contains("ID");
+
+            }
+            catch (Exception err)
+            {
+
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        public List<PoposalModel> SearchProposal(string ethereumContractUser, string ProposalName)
+        {
+            try
+            {
+                string query = "SELECT Proposal.ContracEthereumProposal,Proposal.ProposalName,Proposal.ProposalDescription FROM dbEthereumDemo.Proposal Where (ContracEthereumProposal ='" + ethereumContractUser + "'or '"+ ethereumContractUser + "' is null) and (AND ProposalName='" + ProposalName + "' or '"+ ProposalName + "'=null)";
+
+               List< PoposalModel> proposals = new List<PoposalModel>();
+                MySqlDataReader reader = conexionM.getExecuteQuery(query);
+
+                while (reader.Read())
+                {
+                    PoposalModel proposal = new PoposalModel();
+
+                    proposal.ContracEthereumProposal = reader["ContracEthereumProposal"].ToString();
+                    proposal.ProposalName = reader["ProposalName"].ToString();
+                    proposal.ProposalDescription = reader["ProposalDescription"].ToString();
+
+                    proposals.Add(proposal);
+                }
+
+                return proposals;
+
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+    }
+    public class PoposalModel{
+        public string ContracEthereumProposal { get; set; }
+        public string ProposalName { get; set; }
+        public string ProposalDescription { get; set; }
+    }
+
+}
---
+++ EthereumDemoHost/EthereumWeb/Models/UserConnection.cs
@@ -0,0 +1,163 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EthereumWeb.Models
+{
+    public class UserConnection
+    {
+        Connection conexionM = new Connection();
+
+        public bool UpdateUser(UserModel user)
+        {
+            bool isValid = true;
+
+            try
+            {
+
+              
+
+                List<ParameterSchema> lstParams = new List<ParameterSchema>();
+                string query = string.Empty;
+
+                lstParams.Add(new ParameterSchema("Email", user.Email));
+                lstParams.Add(new ParameterSchema("UserName", user.UserName));
+                lstParams.Add(new ParameterSchema("UserLastName", user.UserLastName));
+                lstParams.Add(new ParameterSchema("IDUser", user.IDUser));
+                lstParams.Add(new ParameterSchema("Password", user.Password));
+                lstParams.Add(new ParameterSchema("Profile", user.Profile));
+
+
+                if (!string.IsNullOrEmpty(user.Email))
+                {
+                    //Inserta Nuevo Curso
+                    query = "UPDATE dbEthereumDemo.User SET" +
+                    "UserName = @UserName,UserLastName = @UserLastName,Password = @Password,Profile = @Profile" +
+                    "WHERE Email = @Email ";
+
+
+                }
+
+                return conexionM.setExecuteQuery(query, lstParams).Contains("ID");
+
+            }
+            catch (Exception err)
+            {
+
+                isValid = false;
+            }
+            return isValid;
+        }
+        public bool InserUser(UserModel user)
+        {
+            bool isValid = true;
+
+            try
+            {
+
+                List<ParameterSchema> lstParams = new List<ParameterSchema>();
+                string query = string.Empty;
+
+                lstParams.Add(new ParameterSchema("Email", user.Email));
+                lstParams.Add(new ParameterSchema("UserName", user.UserName));
+                lstParams.Add(new ParameterSchema("UserLastName", user.UserLastName));
+                lstParams.Add(new ParameterSchema("IDUser", user.IDUser));
+                lstParams.Add(new ParameterSchema("Password", user.Password));
+                lstParams.Add(new ParameterSchema("Profile", user.Profile));
+
+
+                if (!string.IsNullOrEmpty(user.Email))
+                {
+                    //Inserta Nuevo Curso
+                    query = "INSERT INTO dbEthereumDemo.User(Email,UserName,UserLastName,Password,perfil)"+
+                        "VALUES(@Email, @UserName, @UserLastName, @Password, @Profile); ";
+
+
+                }
+              
+
+
+                return conexionM.setExecuteQuery(query, lstParams).Contains("ID");
+
+            }
+            catch (Exception err)
+            {
+
+                isValid = false;
+            }
+            return isValid;
+        }
+        public UserModel GetUserByPassword(string userID, string pass)
+        {
+            try
+            {
+                string query = "SELECT User.EthereumContractUser,User.UserName,User.UserLastName,User.IDUser,User.Password FROM User Where User.IDUser='" + userID + "' AND User.Password='" + pass + "'";
+
+                UserModel user = new UserModel();
+                MySqlDataReader reader = conexionM.getExecuteQuery(query);
+
+                while (reader.Read())
+                {
+                    user.Email = reader["EthereumContractUser"].ToString();
+                    user.IDUser = reader["IDUser"].ToString();
+                    user.Password = reader["Password"].ToString();
+                    user.UserLastName = reader["UserLastName"].ToString();
+                    user.UserName = reader["UserName"].ToString();
+
+
+                }
+
+                return user;
+
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+
+        public UserModel SearchUser(string userID)
+        {
+            try
+            {
+                string query = "SELECT User.EthereumContractUser,User.UserName,User.UserLastName,User.IDUser,User.Password FROM User Where (User.IDUser='" + userID + "' or '"+userID+"'=null)";
+
+                UserModel user = new UserModel();
+                List<UserModel> users = new List<UserModel>();
+                MySqlDataReader reader = conexionM.getExecuteQuery(query);
+
+                while (reader.Read())
+                {
+                    user.Email = reader["EthereumContractUser"].ToString();
+                    user.IDUser = reader["IDUser"].ToString();
+                    user.Password = reader["Password"].ToString();
+                    user.UserLastName = reader["UserLastName"].ToString();
+                    user.UserName = reader["UserName"].ToString();
+                    users.Add(user);
+
+                }
+
+                return user;
+
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+    }
+    public class UserModel
+        {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+        public string IDUser { get; set; }
+        public string Password { get; set; }
+        public string Profile { get; set; }
+    }
+}
+

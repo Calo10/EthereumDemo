81624fe58562ba060afaba631e94193b3116021a
cmendez <carlos.mendez@avantica.net>
2017-09-23 11:38:50 -0600

    Creaete API Dir

---
+++ EthereumDemoApp/EthereumDemoApp/Views/TakePicturePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EthereumDemoApp.Views
+{
+    public partial class TakePicturePage : ContentPage
+    {
+        public TakePicturePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
